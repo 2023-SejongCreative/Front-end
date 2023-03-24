f5a824d6a8b6cd868a87bfa448cfc0056bad767e
judahhh <jdh99080516@gmail.com>
2023-03-24 15:26:41 +0900

    feat : 로그인 분기 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Index";
 import Login from "./pages/login/Index";
 import Register from "./pages/register/Index";
 function App() {
+  localStorage.setItem("isAuthorized", false);
+  let isAuthorized = localStorage.getItem("isAuthorized");
+  console.log(isAuthorized);
   return (
     <div>
       <BrowserRouter>
+        {isAuthorized === false ? (
+          <Navigate to="/login" {...alert("로그인이 필요합니다.")}></Navigate>
+        ) : (
+          <Navigate path="/"></Navigate>
+        )}
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
