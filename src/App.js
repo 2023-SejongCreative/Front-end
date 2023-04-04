42bd24dfb4c94fea379f049054ff10736e4434a7
judahhh <jdh99080516@gmail.com>
2023-04-05 01:59:05 +0900

    feat : 페이지 새로고침 됐을 경우 access 토큰 만료여부를 체크하는 함수를 다시 호출하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import axios from "axios";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Index";
 import Login from "./pages/login/Index";
@@ -6,15 +7,33 @@ import Register from "./pages/register/Index";
 function App() {
   localStorage.setItem("isAuthorized", false);
   let isAuthorized = localStorage.getItem("isAuthorized");
-  //console.log(isAuthorized);
+
+  const onSlientRefresh = () => {
+    axios
+      .post("/reissue", {
+        access_token: localStorage.getItem("jwt_accessToken"),
+        refresh_token: localStorage.getItem("jwt_refreshToken"),
+      })
+      .then((response) => {
+        localStorage.setItem("jwt_accessToken", response.headers.access_token);
+        localStorage.setItem(
+          "jwt_refreshToken",
+          response.headers.refresh_token
+        );
+        setInterval(onSlientRefresh, 1500000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //performance.navigation.type===1
+  if (PerformanceNavigationTiming.type === "reload") {
+    onSlientRefresh();
+  }
+
   return (
     <div>
       <BrowserRouter>
-        {isAuthorized === false ? (
-          <Navigate to="/login" {...alert("로그인이 필요합니다.")}></Navigate>
-        ) : (
-          <Navigate path="/"></Navigate>
-        )}
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
