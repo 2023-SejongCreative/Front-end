0677d7bed3ec40255f82bec4cd0dee413a286bef
judahhh <jdh99080516@gmail.com>
2023-04-05 01:55:46 +0900

    feat : 로그인 성공했을 경우 jwt 로컬스토리지에 저장 및 로그인 연장 기능 구현

--- src/pages/login/Index.js
+++ src/pages/login/Index.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { MyButton, MyTypography } from "./style";
-import api from "../../api/Interceptors";
+import { MyButton, MyTypography, MyLink } from "./style";
+import { api } from "../../api/Interceptors";
 import { useNavigate, NavLink, Link } from "react-router-dom";
-import { MyLink } from "./style";
 import {
-  //Button,
   CssBaseline,
   TextField,
   FormControl,
-  FormControlLabel,
-  Checkbox,
-  //FormHelperText,
   Grid,
   Box,
-  //Typography,
   Container,
 } from "@mui/material/";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Index = () => {
+  const goRegister = () => {
+    navigate("/register");
+  };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -43,23 +40,25 @@ const Index = () => {
       password: password,
     };
     e.preventDefault();
-    // await api
-    //   .post(`/login`, body)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((error) => console.error("Error:", error));
-    await axios
+
+    await api
       .post("/login", body)
       .then((response) => {
         console.log(response);
         if (response.status == 200) {
           alert("로그인 완료! Waffle에 오신걸 환영합니다❤️");
           navigate("/");
-          localStorage.setItem("jwt_accessToken", response.data);
+
+          localStorage.setItem(
+            "jwt_accessToken",
+            response.headers.access_token
+          );
+          localStorage.setItem(
+            "jwt_refreshToken",
+            response.headers.refresh_token
+          );
           localStorage.setItem("isAuthorized", true);
-          console.log(localStorage.getItem("jwt_accessToken"));
-          console.log(localStorage.getItem("isAuthorized"));
+          setTimeout(onSlientRefresh, 1500000);
         } else if (response.response.data.code == "LOGIN-001") {
           alert("일치하는 회원이 없습니다. 먼저 회원가입을 진행해주세요!");
         } else if (response.response.data.code == "LOGIN-002") {
@@ -67,6 +66,25 @@ const Index = () => {
         }
       })
       .catch((error) => console.error("Error:", error));
+  };
+
+  const onSlientRefresh = () => {
+    axios
+      .post("/reissue", {
+        access_token: localStorage.getItem("jwt_accessToken"),
+        refresh_token: localStorage.getItem("jwt_refreshToken"),
+      })
+      .then((response) => {
+        localStorage.setItem("jwt_accessToken", response.headers.access_token);
+        localStorage.setItem(
+          "jwt_refreshToken",
+          response.headers.refresh_token
+        );
+        setInterval(onSlientRefresh, 1500000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -98,8 +116,6 @@ const Index = () => {
                     autoFocus
                     fullWidth
                     type="email"
-                    id="email"
-                    name="email"
                     label="이메일 주소를 입력하세요"
                     value={email}
                     onChange={handleEmailChange}
@@ -110,8 +126,6 @@ const Index = () => {
                     required
                     fullWidth
                     type="password"
-                    id="password"
-                    name="password"
                     label="비밀번호를 입력하세요"
                     value={password}
                     onChange={handlePasswordChange}
@@ -121,7 +135,6 @@ const Index = () => {
               <MyButton
                 type="submit"
                 fullWidth
-                // variant="contained"
                 sx={{ mt: 3, mb: 2 }}
                 size="large"
               >
@@ -129,13 +142,8 @@ const Index = () => {
               </MyButton>
             </FormControl>
           </Box>
-          <MyLink
-            component="button"
-            onClick={() => {
-              navigate("/register");
-            }}
-          >
-            회원가입
+          <MyLink component="button" onClick={goRegister}>
+            회원가입 하러 가기
           </MyLink>
         </Box>
       </Container>
