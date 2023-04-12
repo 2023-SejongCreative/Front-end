239322b8941dfa428d8b187ab65225466adc428d
judahhh <jdh99080516@gmail.com>
2023-04-12 21:51:50 +0900

    feat : rtk가 만료되었을 경우 401 에러 처리

--- src/api/Interceptors.js
+++ src/api/Interceptors.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+// axios.defaults.baseURL = "http://192.168.0.63:8080";
+// axios.defaults.headers.common["access_token"] =
+//   localStorage.getItem("jwt_accessToken");
 export const api = axios.create({
   proxy: {
-    baseURL: `http://172.16.61.131:8080`,
+    baseURL: `http://localhost:4000`,
   },
 
   headers: {
@@ -21,11 +24,11 @@ api.interceptors.request.use(
     // 요청 바로 직전
     // axios 설정값에 대해 작성합니다.
     // console.log(config);
+    config.headers["access_token"] = localStorage.getItem("jwt_accessToken");
     return config;
   },
   function (error) {
     // 요청 에러 처리를 작성합니다.
-    // console.log(error);
     return Promise.reject(error);
   }
 );
@@ -38,7 +41,6 @@ api.interceptors.response.use(
         응답 바로 직전에 대해 작성합니다. 
         .then() 으로 이어집니다.
     */
-    // console.log("get response", response);
     return response;
   },
 
@@ -47,15 +49,43 @@ api.interceptors.response.use(
         http status가 200이 아닌 경우
         응답 에러 처리를 작성합니다.
         .catch() 으로 이어집니다.    
-    */ console.log("rtk 만료되었습니다. 자동 로그아웃 됩니다.", error);
-    const navigate = useNavigate();
-    localStorage.removeItem("jwt_accessToken");
-    localStorage.removeItem("jwt_refreshToken");
-    localStorage.setItem("isAuthorized", false);
-    navigate("/login");
+    */
+    if (error.response && error.response.status === 401) {
+      Logout();
+      return new Promise(() => {}); //이행되지 않은 Promise를 반환하여 Promise Chaining 끊어주기
+    }
+    // console.log("rtk 만료되었습니다. 자동 로그아웃 됩니다.", error);
+    // const navigate = useNavigate();
+    // localStorage.removeItem("jwt_accessToken");
+    // localStorage.removeItem("jwt_refreshToken");
+    // localStorage.setItem("isAuthorized", false);
+    // navigate("/login");
 
     return Promise.reject(error);
   }
 );
 
+const Logout = () => {
+  const navigate = useNavigate();
+  api
+    .post(
+      "/",
+      {},
+      {
+        headers: {
+          access_token: localStorage.getItem("jwt_accessToken"),
+          refresh_token: localStorage.getItem("jwt_refreshToken"),
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      localStorage.removeItem("jwt_accessToken");
+      localStorage.removeItem("jwt_refreshToken");
+      localStorage.setItem("isAuth", false);
+      alert("로그아웃 성공! 다음에 또 만나요❤️");
+      navigate("/login");
+    })
+    .catch((err) => console.log(err));
+};
 //  export default api;
