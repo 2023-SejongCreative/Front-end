dec1edb99c5053ffce2ec884d4be1ee43005373e
judahhh <jdh99080516@gmail.com>
2023-04-12 21:48:02 +0900

    feat : 홈에서 사이드바를 컴포넌트로 분리

--- src/pages/home/Index.js
+++ src/pages/home/Index.js
@@ -1,116 +1,23 @@
 import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 import { api } from "../../api/Interceptors";
+import ModalCreate from "../../components/ModalGroup";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { MyToolbar, HeaderBtn, MyTitle } from "./style";
+import Header from "../../components/header/Header";
+import SideBarAtHome from "../../components/SideBarAtHome";
 
-const drawerWidth = 240;
 const Index = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const getUserData = async () => {
-      const userData = await axios
-        .get("/", {
-          headers: {
-            Authorization: localStorage.getItem("jwt_accessToken"),
-          },
-        })
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => console.log(err));
-    };
-  }, []);
+  // const user_email = useSelector((state) => state.user.email);
+  // // const navigate = useNavigate();
+  // // const dispatch = useDispatch();
 
-  const Logout = () => {
-    api
-      .post(
-        "/",
-        {},
-        {
-          headers: {
-            access_token: localStorage.getItem("jwt_accessToken"),
-            refresh_token: localStorage.getItem("jwt_refreshToken"),
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response);
-        localStorage.removeItem("jwt_accessToken");
-        localStorage.removeItem("jwt_refreshToken");
-        localStorage.setItem("isAuthorized", false);
-        alert("로그아웃 성공! 다음에 또 만나요❤️");
-        navigate("/login");
-      })
-      .catch((err) => console.log(err));
-  };
   return (
     <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <MyToolbar>
-          <HeaderBtn>일정</HeaderBtn>
-          <HeaderBtn>프로필</HeaderBtn>
-          <HeaderBtn>채팅</HeaderBtn>
-        </MyToolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <MyTitle>waffle</MyTitle>
-        <Divider />
-
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <button onClick={Logout}>Logout</button>
-      </Drawer>
+      <Header />
+      <SideBarAtHome />
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
