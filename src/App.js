901b06ca9240eb5d66dc856c75fa72f19fe5643d
judahhh <jdh99080516@gmail.com>
2023-04-12 21:53:07 +0900

    fix : 각 페이지별 파라미터 변수명 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
-import axios from "axios";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { api } from "./api/Interceptors";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Index";
 import Login from "./pages/login/Index";
 import Register from "./pages/register/Index";
+import Group from "./pages/group/Group";
+import Room from "./pages/room/Room";
+// import { useSelector } from "react-redux";
+
 function App() {
-  localStorage.setItem("isAuthorized", false);
-  let isAuthorized = localStorage.getItem("isAuthorized");
+  // let isAuth = useSelector((state) => state.user.isAuth);
 
   const onSlientRefresh = () => {
-    axios
+    api
       .post("/reissue", {
         access_token: localStorage.getItem("jwt_accessToken"),
         refresh_token: localStorage.getItem("jwt_refreshToken"),
@@ -20,7 +22,7 @@ function App() {
           "jwt_refreshToken",
           response.headers.refresh_token
         );
-        setInterval(onSlientRefresh, 1500000);
+        setInterval(onSlientRefresh, 1500000); //25분마다 리이슈 요청
       })
       .catch((err) => {
         console.log(err);
@@ -35,9 +37,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/" isAuth element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
+          <Route path="/group/:group_id" element={<Group></Group>}></Route>
+          <Route path="/room/:room_id" element={<Room></Room>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
