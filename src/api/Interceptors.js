e4c3b676ae9e33f1f5acdf67301057a9d734fae1
judahhh <jdh99080516@gmail.com>
2023-04-20 04:24:28 +0900

    fix : 로그아웃 후 로컬 스토리지에 로그인 여부 false 로 변경

--- src/api/Interceptors.js
+++ src/api/Interceptors.js
@@ -82,7 +82,7 @@ const Logout = () => {
       console.log(response);
       localStorage.removeItem("jwt_accessToken");
       localStorage.removeItem("jwt_refreshToken");
-      localStorage.setItem("isAuth", false);
+      localStorage.setItem("isLogined", false);
       alert("로그아웃 성공! 다음에 또 만나요❤️");
       navigate("/login");
     })
