ac741381e6976abb1d329b0eb941941f1d0e9f7c
judahhh <jdh99080516@gmail.com>
2023-04-20 04:20:50 +0900

    fix : 로컬스토리지 값 이름 변경

--- src/components/sidebar/SideBarAtGroup.js
+++ src/components/sidebar/SideBarAtGroup.js
@@ -154,7 +154,7 @@ const SideBarAtGroup = (props) => {
         console.log(response);
         localStorage.removeItem("jwt_accessToken");
         localStorage.removeItem("jwt_refreshToken");
-        localStorage.setItem("isAuth", false);
+        localStorage.setItem("isLogined", false);
         alert("로그아웃 성공! 다음에 또 만나요❤️");
         navigate("/login");
       })
